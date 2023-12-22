821aed6f92a9056409d925755d8866ade7182536
blackSkyss <blacksky01102001@gmail.com>
2023-12-22 14:46:08 +0700

    feat(project): add insertion, selection, bubble sort

---
+++ sort/AlgSort.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sort
+{
+    public static class AlgSort
+    {
+        // [2, 5, 8, 9, 4]
+        // [0, 1, 2, 3, 4]
+        // insertion
+        public static (List<int>, string) InsertionSort(List<int> ints)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            #region logic
+            for (var i = 1; i < ints.Count; i++)
+            {
+                Console.WriteLine($"---i:{i}---");
+                var j = i;
+                while (j > 0 && ints[j - 1] > ints[j])
+                {
+                    Console.WriteLine($"i:{i}, j:{j}");
+                    (ints[j], ints[j - 1]) = (ints[j - 1], ints[j]);
+                    j -= 1;
+                }
+            }
+            #endregion
+
+            stopwatch.Stop();
+            TimeSpan elapsedTime = stopwatch.Elapsed;
+            return (ints, elapsedTime.TotalMilliseconds.ToString());
+        }
+
+        // [2, 5, 8, 9, 4]
+        // [0, 1, 2, 3, 4]
+        // selection
+        public static (List<int>, string) SelectionSort(List<int> ints)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            #region logic
+            for (var i = 0; i < ints.Count - 1; i++)
+            {
+                Console.WriteLine($"---i:{i}---");
+                var min = i;
+                for (var j = i + 1; j < ints.Count; j++)
+                {
+                    Console.WriteLine($"i:{i}, j:{j}");
+                    if (ints[j] < min) min = j;
+                }
+
+                if (min != i) (ints[i], ints[min]) = (ints[min], ints[i]);
+            }
+            #endregion
+
+            stopwatch.Stop();
+            TimeSpan elapsedTime = stopwatch.Elapsed;
+            return (ints, elapsedTime.TotalMilliseconds.ToString());
+        }
+
+        // [2, 5, 8, 9, 4]
+        // [0, 1, 2, 3, 4]
+        // bubble
+        public static (List<int>, string) BubbleSort(List<int> ints)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            #region logic
+            for (var i = 0; i < ints.Count; i++)
+            {
+                Console.WriteLine($"---i:{i}---");
+                for (var j = 0; j < ints.Count - 1 - i; j++)
+                {
+                    Console.WriteLine($"i:{i}, j:{j}");
+                    if (ints[j] > ints[j + 1]) (ints[j], ints[j + 1]) = (ints[j + 1], ints[j + 1]);
+                }
+            }
+            #endregion
+
+            stopwatch.Stop();
+            TimeSpan elapsedTime = stopwatch.Elapsed;
+            return (ints, elapsedTime.TotalMilliseconds.ToString());
+        }
+    }
+}
---
+++ sort/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sorting Program!!!");
+            var ints = new List<int>() { 2, 5, 8, 9, 4 };
+            Console.WriteLine($"Before sorting: {string.Join(", ", ints)}\n");
+
+            // insertion sort
+            Console.WriteLine("-----Sorting Insertion-----");
+            (List<int>, string) insertionSort = AlgSort.InsertionSort(ints);
+            Console.WriteLine($"After sorting with insertion alg: {string.Join(", ", insertionSort.Item1)}");
+            Console.WriteLine($"Time to run is: {insertionSort.Item2}\n");
+
+            // selection sort
+            Console.WriteLine("-----Sorting Selection-----");
+            (List<int>, string) selectionSort = AlgSort.SelectionSort(ints);
+            Console.WriteLine($"After sorting with selection alg: {string.Join(", ", selectionSort.Item1)}");
+            Console.WriteLine($"Time to run is: {selectionSort.Item2}\n");
+
+            // bubble sort
+            Console.WriteLine("-----Sorting Bubble-----");
+            (List<int>, string) bubbleSort = AlgSort.BubbleSort(ints);
+            Console.WriteLine($"After sorting with bubble alg: {string.Join(", ", bubbleSort.Item1)}");
+            Console.WriteLine($"Time to run is: {bubbleSort.Item2}");
+        }
+    }
+}
